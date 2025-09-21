76e7b79e8d3fac64329f0f7bfbc392eda9a10d30
fpirotti <fpirotti@gmail.com>
2025-09-21 10:09:48 +0200

    go

--- giorno01.R
+++ giorno01.R
@@ -9,7 +9,8 @@ acf(meuse$zinc, main = "Zinco")
 
 
 
-
+## carichiamo la libreria sf che serve per trasformare le coordinate tra
+## sistemi di riferimento (CRS) differenti
 library(sf)
 lat<-45.398557
 long<-11.876460
@@ -28,22 +29,29 @@ geocentric = st_coordinates(pt_ecef)
 
 
 
-
-
-
+library(sp)
 library(gstat)
 library(terra)
+### carichiamo i dati dell'area meuse - fanno parte del pacchetto sp
 data(meuse.grid)
 data(meuse)
-## molto importanti i passaggi sotto
+## molto importanti i passaggi sotto - impostano le variabili meuse e meuse grid,
+## associate a tabelle, come dati di tipo spatial - meuse.grid come dato spatial
+## a passo regolare
 coordinates(meuse) = ~x+y
 coordinates(meuse.grid) = ~x+y
 gridded(meuse.grid) = TRUE
-###
+summary(meuse.grid)
+meuse.grid.terra <- terra::rast(meuse.grid)
+
+### IDW - dal manuale:### Irast()DW - dal manuale:
+### Function idw performs just as krige without a model being passed,
+### but allows direct specification of the inverse distance weighting power.
+### Don't use with predictors in the formula
 zinc.idw = idw(zinc~1, meuse, meuse.grid)
 plot(zinc.idw)
 
-dev.off()
+
 
 zinc.idw.terra <- terra::rast(zinc.idw)
 plot(zinc.idw.terra, main="Mappa Concentrazione Zinco")

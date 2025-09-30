78f1c5494596ceb7e9991912162a534b05c3f405
fpirotti <fpirotti@gmail.com>
2025-09-30 08:29:36 +0200

    fin

--- giorno02.R
+++ giorno02.R
@@ -2,6 +2,16 @@ library(stars)
 library(terra)
 library(sf)
 library(sp)
+library(gstat)
+
+### carichiamo i dati  dal pacchetto sp
+data(meuse.grid)
+data(meuse)
+coordinates(meuse) = ~x+y
+coordinates(meuse.grid) = ~x+y
+gridded(meuse.grid) = TRUE
+
+
 ### input IDW: meuse sono i dati (locations) e newdata sono le coordinate dove stimare il valore di concentrazione di zinco
 zinc.idw = idw(formula=zinc~1, locations= meuse, newdata=meuse.grid)
 ## trasformo in oggetto "terra" funziona?
@@ -10,20 +20,23 @@ zinc.idw.terra = idw(formula=zinc~1, locations= meuse, newdata=meuse.grid.terra)
 ## trasformo in oggetto "stars" funziona?
 meuse.grid.stars <- stars::st_as_stars(meuse.grid)
 zinc.idw.stars = idw(formula=zinc~1, locations= meuse, newdata=meuse.grid.stars)
-plot(zinc.idw.stars)
-print(zinc.idw.stars)
 
+## trasformo in oggetto "vector" funziona?
+meuse.grid.terra.vect <-  as.points(meuse.grid.terra)  # coordinate
+zinc.idw.punto.vect = idw(formula=zinc~1, locations= meuse, newdata=meuse.grid.terra.vect)
 ## trasformo in oggetto "sf" funziona?
-grid_df <- as.points(meuse.grid.terra)  # coordinate
+meuse.grid.sf <-  sf::st_as_sf(meuse.grid.terra.vect)
+zinc.idw.punto.vect = idw(formula=zinc~1, locations= meuse, newdata=meuse.grid.sf)
+
 ## funziona solo con oggetti classe Spatial, sf o stars  (vedi  manuale)
-zinc.idw.punto = idw(formula=zinc~1, locations= meuse, newdata=grid_df)
-# plot(zinc.idw)
+
 
 ## prendiamo una coordinata, la trasformiamo in sf e la usiamo per stimare conc. zinco
 ## per questo però dobbiamo usare la libreria terra!
-## NB
+## NB disegno covariata "dist" nel raster
 plot(meuse.grid.terra$dist)
-p <- click(meuse.grid.terra, n=1, xy=TRUE, cell=TRUE)
+plot(terra::vect(meuse), "zinc")
+p <- click(meuse.grid.terra, n=1, xy=TRUE)
 print(p)
 p_sf <- sf::st_as_sf(p, coords = c("x", "y"), crs = crs(meuse.grid.terra))
 zinc.idw.punto = idw(formula=zinc~1, locations= meuse, newdata=p_sf)
@@ -34,6 +47,23 @@ print(zinc.idw.punto)
 ## NB - la funzione idw definisce i primi due argomenti, dunque quello fornito
 ## da |> diventa quello successivo, ovvero "newdata"
 ##  modificate pure il vaore di "n" per raccogliere più punti!
-click(meuse.grid.terra, n=1, xy=TRUE, cell=TRUE) |>
-      sf::st_as_sf( coords = c("x", "y"), crs = crs(meuse.grid.terra)) |>
-      idw(formula=zinc~1, locations= meuse)
+##
+
+## ============ EXTRA -
+plot(zinc~1, data= meuse)
+zinc.idw = idw(formula=zinc~1, locations= meuse, newdata=meuse.grid)
+spplot(zinc.idw["var1.pred"], main = "IDW Zinco")
+## covariata distanza  - creo modello lineare
+lm_fit <- lm(log(zinc) ~ sqrt(dist), data = meuse)
+meuse$resid <- residuals(lm_fit)
+## IDW dei residui
+idw_res <- idw(resid ~ 1, meuse, meuse.grid)
+## stimo dal modello lineare la concentrazione zinco
+meuse.grid$trend <- predict(lm_fit, newdata = as.data.frame(meuse.grid))
+## aggiungo i residui stimati
+meuse.grid$logzinc_idwreg <- meuse.grid$trend + idw_res$var1.pred
+meuse.grid$zinc_idwreg <- exp(meuse.grid$logzinc_idwreg)
+
+spplot(meuse.grid["zinc_idwreg"], main = "IDW Zinco con covariata distanza")
+
+

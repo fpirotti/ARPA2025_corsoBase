83638e95f30679a4a666d789578535b876ea074f
fpirotti <fpirotti@gmail.com>
2025-11-29 10:14:22 +0100

    update

--- esercizi/modulo02/giorno3meuse.R
+++ esercizi/modulo02/giorno3meuse.R
@@ -23,11 +23,11 @@ plot(v.dir, v.anis)
 kriging[["OK.anis"]] <-  gstat(NULL, "logZn", formula = f, data=meuse, model=v.anis)
 
 ## UK con X e Y ------
-f <- log(zinc) ~ poly(x,y)
+f <- log(zinc) ~ poly(x,y, degree = 2)
 vt <- variogram(f, meuse)
 vt.fit <- fit.variogram(vt, vgm(1, "Exp", 300, 1))
-# plot(vt, vt.fit)
-kriging[["UK.xy"]] <-  gstat(NULL, "logZn.polyXY", formula = f, data=meuse, model=vt.fit)
+plot(vt, vt.fit)
+kriging[["UK.poly2degxy"]] <-  gstat(NULL, "logZn.polyXY", formula = f, data=meuse, model=vt.fit)
 
 ## UK con dist ------
 f <- log(zinc) ~ sqrt(dist)
@@ -68,18 +68,18 @@ vov <- var(meuse.grid$distn)/var(log(meuse$zinc))
 vd.fit$psill <- v.fit$psill * vov
 
 # Aggiunge distn come nuova variabile gstat
-g.cc <- gstat(g.cc, "distn", distn ~ 1, meuse.grid, nmax = 1, model = vd.fit, merge = c("log.zinc", "distn"))
+g.cc <- gstat(g.cc, "distn", distn ~ 1, meuse.grid, nmax = 1, model = vd.fit, merge = c("logZn.Colloc", "distn"))
 # nmax = 1 significa considerare solo il vicino più prossimo per la variabile secondaria
-# merge = c("log.zinc", "distn") combina le strutture dati per il cokriging
+# merge = c("logZn.Colloc", "distn") combina le strutture dati per il cokriging
 
 ####### Variogramma incrociato per il cokriging
 vx.fit <- v.fit
 vx.fit$psill <- sqrt(v.fit$psill * vd.fit$psill) * cor(meuse$dist, log(meuse$zinc))
-g.cc <- gstat(g.cc, c("log.zinc", "distn"), model = vx.fit)
+g.cc <- gstat(g.cc, c("logZn.Colloc", "distn"), model = vx.fit)
 kriging[["COK.coll"]] <- g.cc
 
-## BLOCK KRIGING ----- applicato quando si va ad applicare il modello
-predict(kriging[["OK"]], meuse.grid, block = c(30,30))
+## BLOCK KRIGING ----- viene applicato quando si va ad applicare il modello!
+#  predict(kriging[["OK"]], meuse.grid, block = c(30,30))
 
 ## Kriging variabile discreta -----
 v <- variogram(log(zinc) ~ soil, meuse)
@@ -92,12 +92,14 @@ kriging[["UK.cat"]] <- gstat(NULL, "logZn.soil", formula = log(zinc) ~ soil, dat
 gstat2predict <- function(ogg.gstat){
 
   # r <- predict(g, meuse.grid)
-
+  # browser()
   ## attenzione in co-kriging prende solo la prima variabile! ----
   ## uso 5 folds ... i dati vengono divisi in 5 gruppi (“fold”).
   ## 1 viene usato per validazione e i 4 rimanenti per aggiustare
   cv <- tryCatch({
-    gstat.cv(ogg.gstat, nfold = 5)
+    gstat.cv(ogg.gstat, nfold = 5 )
+    ## sostituite la riga sopra con quella sotto per fare "block kriging"
+    # gstat.cv(ogg.gstat, nfold = 5, block=c(50,50) )
   }, error=function(e){
     browser()
     message(e)
@@ -114,7 +116,7 @@ gstat2predict <- function(ogg.gstat){
   #      xlab="Osservati", ylab="Stimati")
   ME <- mean(cv$residual)
   RMSE <- sqrt(mean(cv$residual^2)) ### attenzione, NON sd(cv$residual)
-  browser()
+
   # Residui standardizzati
   MSDR <- mean(cv$zscore^2)
 
@@ -126,7 +128,7 @@ gstat2predict <- function(ogg.gstat){
 predizioni <- lapply(kriging,   gstat2predict)
 
 validazione <- data.table::rbindlist(predizioni,idcol = "tipo")
-
+View(validazione)
 
 
 
